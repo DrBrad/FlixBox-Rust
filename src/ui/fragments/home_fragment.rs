ee1deae1b61366114df5a9147db72226c98c9326
DrBrad <brad@bradeagle.com>
2024-02-07 22:42:44 -0700

    Traits

--- src/ui/fragments/home_fragment.rs
+++ src/ui/fragments/home_fragment.rs
@@ -1,5 +1,7 @@
-use gtk::{Align, ButtonExt, ContainerExt, CssProvider, CssProviderExt, GtkWindowExt, Image, ImageExt, OrientableExt, Orientation, WidgetExt, Window, WindowType};
+use gtk::prelude::*;
+use gtk::{Align, ButtonExt, ContainerExt, CssProvider, CssProviderExt, Fixed, FixedExt, GtkWindowExt, Image, ImageExt, OrientableExt, Orientation, WidgetExt, Window, WindowType};
 use gdk_pixbuf::Pixbuf;
+use gtk::Align::{Fill, Start};
 use crate::ui::int::fragment::Fragment;
 
 pub struct HomeFragment<'a> {
@@ -9,85 +11,47 @@ pub struct HomeFragment<'a> {
 
 impl<'a> HomeFragment<'a> {
 
-    fn add_header(root_pane: &gtk::Box, scroll_root: &gtk::Box){
-        //let root = gtk::Box::new(Orientation::Horizontal, 0);
-        //root.set_hexpand(true);
-        //root.set_widget_name("header");
+    fn add_header(root_pane: &gtk::ScrolledWindow, list_box: &gtk::Box){
+        let parent = gtk::AspectFrame::new(None, 0.5, 0.5, 16.0 / 9.0, false);
+        //parent.set_property_height_request(100);
+        parent.set_hexpand(true);
+        parent.set_vexpand(true);
+        //parent.set_valign(Start);
+        //parent.set_halign(Fill);
+        parent.set_widget_name("header");
 
-        //let root = gtk::AspectFrame::new(None, 0.5, 0.5, 16.0 / 9.0, true);
-        //root.set_valign(Align::Start);
-        //root.set_hexpand(true);
-        //root.set_vexpand(true);
+        let fixed = gtk::Fixed::new();//(Orientation::Horizontal, 0);
+        fixed.set_hexpand(true);
+        fixed.set_vexpand(true);
+        //fixed.set_valign(Fill);
+        //fixed.set_halign(Fill);
+        //parent.set_widget_name("fixed");
+        parent.add(&fixed);
 
-        let root = gtk::Box::new(Orientation::Horizontal, 0);
-        root.set_hexpand(true);
-        root.set_vexpand(true);
-        root.set_widget_name("header");
-        //root.add(&root);
-        scroll_root.add(&root);
+        let b = gtk::Button::new();
+        b.set_hexpand(true);
+        b.set_vexpand(true);
+
+        fixed.put(&b, 150, 150);
+
+        list_box.add(&parent);
 
         let aspect_ratio = 16.0 / 9.0;
 
         root_pane.connect_size_allocate(move |_, allocation| {
-            println!("{}, {}", allocation.width, allocation.height);
             let width = allocation.width as f64;
             let height = width / aspect_ratio;
-            root.set_size_request(allocation.width, height as i32);
+            parent.set_size_request(allocation.width, height as i32);
+            parent.queue_resize();
         });
-
-        /*
-        let pixbuf = Pixbuf::from_file("/var/www/images/landscape/d04f148795a31c4c0723e49fe635ec5e297aba89a76ae73060eb1b031609f7fd.jpg").unwrap();
-        //let scaled_pixbuf = pixbuf.scale_simple(960, 540, gdk_pixbuf::InterpType::Hyper).unwrap(); // Adjust the dimensions as needed
-
-        //image.set_from_pixbuf(Some(&pixbuf));
-        //image.set_from_pixbuf(Some(&Pixbuf::from_file("/var/www/images/landscape/d04f148795a31c4c0723e49fe635ec5e297aba89a76ae73060eb1b031609f7fd.jpg").unwrap()));
-        root.add(&image);
-
-        root.connect_size_allocate(move |_, allocation| {
-            println!("{}, {}", image.get_allocated_width(), image.get_allocated_height());
-
-            let width = allocation.width as i32;
-            let height = allocation.height as i32;
-
-            // Calculate the scaling dimensions while preserving the aspect ratio
-            let (scaled_width, scaled_height) = if width * pixbuf.get_height() > height * pixbuf.get_width() {
-                let new_height = height;
-                let new_width = (new_height * pixbuf.get_width()) / pixbuf.get_height();
-                (new_width, new_height)
-            } else {
-                let new_width = width;
-                let new_height = (new_width * pixbuf.get_height()) / pixbuf.get_width();
-                (new_width, new_height)
-            };
-
-            let scaled_pixbuf = pixbuf.scale_simple(scaled_width-2, scaled_height-2, gdk_pixbuf::InterpType::Hyper).unwrap();
-            image.set_from_pixbuf(Some(&scaled_pixbuf));
-        });
-        */
-
-        /*
-        image.connect_size_allocate(move |_, allocation| {
-            let pixbufa = pixbuf.clone();
-            // Get the new width and height of the window
-            let width = allocation.width;
-            let height = allocation.height;
-
-            // Scale the image to fit the new window size
-            let new_pixbuf = pixbufa.scale_simple(width, height, gdk_pixbuf::InterpType::Hyper).unwrap();
-            image.set_from_pixbuf(Some(&new_pixbuf));
-        });
-        */
-
-        //scroll_root.add(&root);
-
     }
 
-    fn add_list(scroll_root: &gtk::Box){
+    fn add_list(list_box: &gtk::Box){
         let root = gtk::Box::new(Orientation::Horizontal, 0);
         root.set_property_width_request(200);
         root.set_property_height_request(500);
 
-        scroll_root.add(&root);
+        list_box.add(&root);
     }
 }
 
@@ -110,55 +74,17 @@ impl<'a> Fragment<'a> for HomeFragment<'a> {
         scrolled_pane.set_vexpand(true);
         scrolled_pane.set_hexpand(true);
 
-        let scrolled_root = gtk::Box::new(Orientation::Vertical, 0);
-        //scrolled_root.set_valign(Align::Start);
-        scrolled_pane.add(&scrolled_root);
-        //scrolled_window.set_hscrollbar_policy(gtk::PolicyType::Always);
-        //scrolled_window.set_vscrollbar_policy(gtk::PolicyType::Automatic);
+        let list_box = gtk::Box::new(Orientation::Vertical, 0);
+        list_box.set_vexpand(true);
+        list_box.set_hexpand(true);
+        scrolled_pane.add(&list_box);
 
-        /*
-        let list_box = gtk::ListBox::new();
-
-        // Simulate a dataset with 100 items
-        for i in 0..100 {
-            // Create a label for each item
-            let label = gtk::Label::new(Some(&format!("Item {}", i)));
-
-            // Add the label to the list box
-            list_box.add(&label);
-        }
-
-        scrolled_window.add(&list_box);
-        */
-
-        HomeFragment::add_header(&self.root_pane, &scrolled_root);
-        HomeFragment::add_list(&scrolled_root);
+        HomeFragment::add_header(&scrolled_pane, &list_box);
+        HomeFragment::add_list(&list_box);
+        HomeFragment::add_list(&list_box);
+        HomeFragment::add_list(&list_box);
 
         self.root_pane.add(&scrolled_pane);
-
-        /*
-        let splash = gtk::Box::new(Orientation::Horizontal, 0);
-        splash.set_widget_name("splash");
-        //splash.set_hexpand(true);
-        //splash.set_vexpand(false);
-        //splash.set_property_height_request(300);
-
-
-
-        let aspect_ratio = 8.0 / 2.0; // Desired aspect ratio (width / height)
-        let width = 400; // Desired width
-        let height = (width as f64 / aspect_ratio) as i32; // Calculate height based on aspect ratio
-        splash.set_size_request(width, height);
-
-        // Connect to the "size-allocate" signal to handle resizing
-        splash.connect_size_allocate(move |widget, allocation| {
-            let allocation_width = allocation.width;
-            let allocation_height = (allocation_width as f64 / aspect_ratio) as i32;
-            widget.set_size_request(allocation_width, allocation_height);
-        });
-
-        self.root_pane.add(&splash);
-        */
     }
 
     fn get_parent(&self) -> &'a gtk::Box {
